5bd57f817b03e41dac00652444ef5c6e70c98bfe
Thanh Nguyen Le <nlethanh@tma.com.vn>
2023-07-11 16:06:09 +0700

    test postgresql 05

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,8 +34,4 @@ File { backup => false }
 #   }
 # }
 class { 'postgresql::server':
-  ip_mask_deny_postgres_user => '0.0.0.0/32',
-  ip_mask_allow_all_users    => '0.0.0.0/0',
-  ipv4acls                   => ['hostssl all johndoe 192.168.0.0/24 cert'],
-  postgres_password          => 'Thanh@1101@',
 }
